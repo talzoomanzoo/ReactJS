ad071774fbfb61b5a0160a524777222e61a0eddc
minju <mjgwakk@gmail.com>
2023-09-09 18:01:55 +0900

    0909

--- twitter-youtube/src/main/java/com/talzoomanzoo/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.talzoomanzoo.config;
-
-public class JwtConstant {
-	
-	public static final String SECRET_KEY="sdfljsdlfskfkqjwlkqjflkjslkdjflsk";
-	public static final String JWT_HEADER="Authorization";
-}
--- twitter-youtube/src/main/java/com/talzoomanzoo/exception/RibbitException.java
+++
@@ -1,8 +0,0 @@
-package com.talzoomanzoo.exception;
-
-public class RibbitException extends Exception {
-
-	public RibbitException(String message) {
-		super (message);
-	}
-}
